5e11eb42eb1588b752003944f4527dcb3c038ffa
Michal Pasierbski <mpasierb@amazon.com>
2021-03-11 13:03:52 +0100

    Update wdio to v7

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 import DeviceFarmLauncher from "./launcher";
 
-export default class DeviceFarmService {}
 export const launcher = DeviceFarmLauncher;
--- src/launcher.ts
+++ src/launcher.ts
@@ -1,46 +1,46 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import WebDriver from "webdriver";
-import WebdriverIO, { SevereServiceError } from "webdriverio";
+import { SevereServiceError } from "webdriverio";
 import DeviceFarm from "aws-sdk/clients/devicefarm";
 import getLogger from "@wdio/logger";
+import { Services, Capabilities, Options } from "@wdio/types";
 
 const log = getLogger("@wdio/devicefarm-service");
 
-interface DeviceFarmConfig {
+interface DeviceFarmConfig extends Services.ServiceOption {
   projectArn: string;
   expiresInSeconds?: number;
 }
 
-export default class DeviceFarmLauncher implements WebdriverIO.HookFunctions {
+export default class DeviceFarmLauncher implements Services.ServiceInstance {
   private readonly devicefarm: DeviceFarm;
-  private readonly options: DeviceFarmConfig;
 
-  constructor(options: DeviceFarmConfig) {
+  constructor(private readonly _options: DeviceFarmConfig) {
     // DeviceFarm is only available in us-west-2
     // https://docs.aws.amazon.com/general/latest/gr/devicefarm.html
     this.devicefarm = new DeviceFarm({ region: "us-west-2" });
-    this.options = options;
   }
 
   public async onPrepare(
-    _config: WebdriverIO.Config,
-    capabilities: WebDriver.DesiredCapabilities[]
+    _config: Options.Testrunner,
+    capabilities: Capabilities.RemoteCapabilities
   ): Promise<void> {
-    for (const cap of capabilities) {
-      const testGridUrlResult = await this.createSession();
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const url = new URL(testGridUrlResult.url!);
+    if (Array.isArray(capabilities)) {
+      for (const cap of capabilities) {
+        const testGridUrlResult = await this.createSession();
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const url = new URL(testGridUrlResult.url!);
 
-      log.info("Created device farm test grid:", testGridUrlResult);
+        log.info("Created device farm test grid:", testGridUrlResult);
 
-      Object.assign(cap, {
-        protocol: "https",
-        port: 443,
-        hostname: url.hostname,
-        path: url.pathname,
-        connectionRetryTimeout: 180000,
-      });
+        Object.assign(cap, {
+          protocol: "https",
+          port: 443,
+          hostname: url.hostname,
+          path: url.pathname,
+          connectionRetryTimeout: 180000,
+        });
+      }
     }
   }
 
@@ -49,8 +49,8 @@ export default class DeviceFarmLauncher implements WebdriverIO.HookFunctions {
     try {
       return await this.devicefarm
         .createTestGridUrl({
-          projectArn: this.options.projectArn,
-          expiresInSeconds: this.options.expiresInSeconds || 900,
+          projectArn: this._options.projectArn,
+          expiresInSeconds: this._options.expiresInSeconds || 900,
         })
         .promise();
     } catch (err) {
