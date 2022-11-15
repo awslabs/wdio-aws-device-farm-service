c5c91572c2589e47d3e190c881001f3fa7e6eaf3
Florian Nagel <nagelflorian@users.noreply.github.com>
2022-11-15 15:42:17 +0100

    Upgrade various dev dependencies (#27)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname,
-    },
-    env: {
-        jest: true,
-        node: true,
-    },
-    plugins: ['@typescript-eslint'],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-    ],
-}
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    jest: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+};
