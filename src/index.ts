79088a336ac7d003f0c0800990397ec1ae26ca49
Javier Luna Molina <4939482+JavierLuna@users.noreply.github.com>
2026-04-09 12:04:41 +0200

    Add support for wdio 9 (#110)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import DeviceFarmLauncher from "./launcher";
+import DeviceFarmLauncher from "./launcher.js";
 
 export const launcher = DeviceFarmLauncher;
